980e096a79432ab10ee898d2264ef97411f53dda
klueless-io <david@ideasmen.com.au>
2021-02-25 13:06:52 +1100

    first commit

---
+++ lib/webpack5/builder.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "builder/version"
+
+module Webpack5
+  module Builder
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/webpack5/builder_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Webpack5::Builder do
+  it "has a version number" do
+    expect(Webpack5::Builder::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
